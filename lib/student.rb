ef7354f9b951205f4d224793a4bc1d460253c5f1
Tyler Hunt <tyhunt04@gmail.com>
2019-03-17 18:42:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,8 +42,8 @@ class Student
 
   def self.students_below_12th_grade
     sql = <<-SQL
-    SELECT students.name FROM students
-    WHERE grade < 12
+    SELECT * FROM students
+    WHERE grade > 12
     SQL
 
     DB[:conn].execute(sql)
