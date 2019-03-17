bdeca5f1c948c9a514e3b991d3f3b1466787e8e6
Tyler Hunt <tyhunt04@gmail.com>
2019-03-17 18:54:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,7 +68,7 @@ class Student
   def self.first_student_in_grade_10
     sql = <<-SQL
     SELECT * FROM students
-    ORDER BY students.id DECS
+    ORDER BY students.id DESC
     WHERE grade = 10
     LIMIT 1
     SQL
