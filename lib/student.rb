a2d49c263775b28239ac17f418d744d975e67e83
Tyler Hunt <tyhunt04@gmail.com>
2019-03-17 18:56:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -72,9 +72,9 @@ class Student
     LIMIT 1
     SQL
 
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, 10).map do |row|
       self.new_from_db(row)
-    end
+    end.first
   end
 
   def self.all_students_in_grade_X(num)
